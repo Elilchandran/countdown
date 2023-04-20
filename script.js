51d9f3dabc854cd194a59d9dbb016de5ebc85ad6
Elilarasi <127470628+Elilchandran@users.noreply.github.com>
2023-04-21 01:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(function() {
+  document.getElementById('countdown').innerHTML = '10';
+  setTimeout(function() {
+    document.getElementById('countdown').innerHTML = '9';
+    setTimeout(function() {
+      document.getElementById('countdown').innerHTML = '8';
+      setTimeout(function() {
+        document.getElementById('countdown').innerHTML = '7';
+        setTimeout(function() {
+          document.getElementById('countdown').innerHTML = '6';
+          setTimeout(function() {
+            document.getElementById('countdown').innerHTML = '5';
+            setTimeout(function() {
+              document.getElementById('countdown').innerHTML = '4';
+              setTimeout(function() {
+                document.getElementById('countdown').innerHTML = '3';
+                setTimeout(function() {
+                  document.getElementById('countdown').innerHTML = '2';
+                  setTimeout(function() {
+                    document.getElementById('countdown').innerHTML = '1';
+                    setTimeout(function(){
+                      document.getElementById('countdown').innerHTML='';
+                    setTimeout(function() {
+                      document.getElementById('message').innerHTML = 'Happy Independence Day';
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+},1000);
